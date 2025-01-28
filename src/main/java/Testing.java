56f41d856fc13e4f9888f6f64551f5196be62a83
Suresh Thammishetty <suresh.thammishetti@gmail.com>
2025-01-28 22:02:24 +0530

    Local to GIT check

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -4,5 +4,6 @@ public class Testing {
   public static void main(String a[]){
     System.out.println("This is my commit from GIT");
     System.out.println("This commit is from My Local");
+    System.out.println("This commit is from My Testing 1234");
   }
 }
