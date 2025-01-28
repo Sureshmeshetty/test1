a0dc74624d1aece6aea56c11edd14baf3706d16c
Suresh Thammishetty <suresh.thammishetti@gmail.com>
2025-01-28 22:01:37 +0530

    Update Testing.java

--- src/main/java/Testing.java
+++ src/main/java/Testing.java
@@ -3,6 +3,8 @@ package main.java;
 public class Testing {
   public static void main(String a[]){
     System.out.println("This is my commit from GIT");
+    System.out.println("This commit is from My Local123");
     System.out.println("This commit is from My Local");
+    
   }
 }
